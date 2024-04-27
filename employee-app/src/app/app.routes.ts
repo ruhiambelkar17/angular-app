de0979b9d34636e5a3f4790ac112723cdc6df0f7
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-04-27 13:46:52 +0200

    cloned and created angular project

---
+++ employee-app/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+
+export const routes: Routes = [
+    {path:'',component:EmployeeDashboardComponent}
+];
+
+// @NgModule({
+//     imports:[RouterModule.forRoot(routes)],
+//     exports:[RouterModule]
+// })
+
+
+
