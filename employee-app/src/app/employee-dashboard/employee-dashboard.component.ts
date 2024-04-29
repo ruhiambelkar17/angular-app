bc7a213a8dca8084bd759f5d78f14b349e27da6b
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-04-29 06:54:41 +0200

    removed unwanted code

--- employee-app/src/app/employee-dashboard/employee-dashboard.component.ts
+++ employee-app/src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
-import {CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from '../services/data.service';
-import { CommonModule } from '@angular/common'
-import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-employee-dashboard',
   standalone: true,
-  imports: [CommonModule,HttpClientModule,MatMenuModule, MatCardModule,MatButtonModule, MatSelectModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatMenuModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './employee-dashboard.component.html',
   styleUrl: './employee-dashboard.component.css',
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class EmployeeDashboardComponent implements OnInit {
   posts: any[] = [];
@@ -33,24 +42,22 @@ export class EmployeeDashboardComponent implements OnInit {
   loadPosts() {
     this.dataService.getPosts().subscribe((data) => {
       this.posts = data.data;
-      console.log("**api response",this.posts[0].attributes)
+      console.log('**api response', this.posts[0].attributes);
     });
   }
 
   addPost() {
-    console.log("add post hitted******")
-    const newPost = {data: { empId: 1002, empName:'Emp2', empEmail: 'emp2@gmail.com' }};
-    
+    const newPost = {
+      data: { empId: 1002, empName: 'Emp2', empEmail: 'emp2@gmail.com' },
+    };
     this.dataService.createPost(newPost).subscribe(() => {
       this.posts.unshift(newPost);
     });
     location.reload();
-    
   }
 
   updatePost(post: any) {
-    console.log("update hitted****",post)
-    const updatedPost = { ...post, data:{empEmail: 'emp22@gmail.com'} };
+    const updatedPost = { ...post, data: { empEmail: 'emp22@gmail.com' } };
     this.dataService.updatePost(post, updatedPost).subscribe(() => {
       const index = this.posts.findIndex((p) => p.id === post.id);
       this.posts[index] = updatedPost;
